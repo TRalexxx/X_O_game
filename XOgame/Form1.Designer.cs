fde9068a6a9764c346ff38e540dec50a0301f4bb
TRalexxx <110291348+TRalexxx@users.noreply.github.com>
2023-03-26 11:58:01 +0300

    init commit
    Created window for game

---
+++ XOgame/Form1.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace XOgame
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 301);
+            this.Text = "X 0 Game";
+            this.ResumeLayout(false);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        }
+
+    }
+}
+
