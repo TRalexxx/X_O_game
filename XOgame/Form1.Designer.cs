b8984899fb0467a7b93e3116a2062727bccd3fd2
TRalexxx <110291348+TRalexxx@users.noreply.github.com>
2023-03-26 12:06:51 +0300

    Update Form1.Designer.cs
    Add set X and O to fields on click

--- XOgame/Form1.Designer.cs
+++ XOgame/Form1.Designer.cs
@@ -35,6 +35,13 @@ namespace XOgame
             btnOne.Size = new Size(100, 100);
             btnOne.Location = new Point(0, 0);
             btnOne.Font = new Font("Times New Roman", 28);
+            btnOne.Click += (e, sender) =>
+            {
+                if (click) btnOne.Text = "X";
+                else btnOne.Text = "0";
+                click = !click;
+                btnOne.Enabled = false;
+            };
             //
             // Second field
             //
@@ -42,6 +49,13 @@ namespace XOgame
             btnTwo.Size = new Size(100, 100);
             btnTwo.Location = new Point(101, 0);
             btnTwo.Font = new Font("Times New Roman", 28);
+            btnTwo.Click += (e, sender) =>
+            {
+                if (click) btnTwo.Text = "X";
+                else btnTwo.Text = "0";
+                click = !click;
+                btnTwo.Enabled = false;
+            };
             //
             // Third field
             //
@@ -49,6 +63,13 @@ namespace XOgame
             btnThree.Size = new Size(100, 100);
             btnThree.Location = new Point(201, 0);
             btnThree.Font = new Font("Times New Roman", 28);
+            btnThree.Click += (e, sender) =>
+            {
+                if (click) btnThree.Text = "X";
+                else btnThree.Text = "0";
+                click = !click;
+                btnThree.Enabled = false;
+            };
             //
             // Fourth field
             //
@@ -56,6 +77,13 @@ namespace XOgame
             btnFour.Size = new Size(100, 100);
             btnFour.Location = new Point(0, 101);
             btnFour.Font = new Font("Times New Roman", 28);
+            btnFour.Click += (e, sender) =>
+            {
+                if (click) btnFour.Text = "X";
+                else btnFour.Text = "0";
+                click = !click;
+                btnFour.Enabled = false;
+            };
             //
             // Fifth field
             //
@@ -63,6 +91,13 @@ namespace XOgame
             btnFive.Size = new Size(100, 100);
             btnFive.Location = new Point(101, 101);
             btnFive.Font = new Font("Times New Roman", 28);
+            btnFive.Click += (e, sender) =>
+            {
+                if (click) btnFive.Text = "X";
+                else btnFive.Text = "0";
+                click = !click;
+                btnFive.Enabled = false;
+            };
             //
             // Sixth field
             //
@@ -70,6 +105,13 @@ namespace XOgame
             btnSix.Size = new Size(100, 100);
             btnSix.Location = new Point(201, 101);
             btnSix.Font = new Font("Times New Roman", 28);
+            btnSix.Click += (e, sender) =>
+            {
+                if (click) btnSix.Text = "X";
+                else btnSix.Text = "0";
+                click = !click;
+                btnSix.Enabled = false;
+            };
             //
             // Seventh field
             //
@@ -77,6 +119,13 @@ namespace XOgame
             btnSeven.Size = new Size(100, 100);
             btnSeven.Location = new Point(0, 201);
             btnSeven.Font = new Font("Times New Roman", 28);
+            btnSeven.Click += (e, sender) =>
+            {
+                if (click) btnSeven.Text = "X";
+                else btnSeven.Text = "0";
+                click = !click;
+                btnSeven.Enabled = false;
+            };
             //
             // Eighth field
             //
@@ -84,6 +133,13 @@ namespace XOgame
             btnEight.Size = new Size(100, 100);
             btnEight.Location = new Point(101, 201);
             btnEight.Font = new Font("Times New Roman", 28);
+            btnEight.Click += (e, sender) =>
+            {
+                if (click) btnEight.Text = "X";
+                else btnEight.Text = "0";
+                click = !click;
+                btnEight.Enabled = false;
+            };
             //
             // Ninth field
             //
@@ -91,6 +147,13 @@ namespace XOgame
             btnNine.Size = new Size(100, 100);
             btnNine.Location = new Point(201, 201);
             btnNine.Font = new Font("Times New Roman", 28);
+            btnNine.Click += (e, sender) =>
+            {
+                if (click) btnNine.Text = "X";
+                else btnNine.Text = "0";
+                click = !click;
+                btnNine.Enabled = false;
+            };
 
             this.Controls.Add(btnOne);
             this.Controls.Add(btnTwo);
