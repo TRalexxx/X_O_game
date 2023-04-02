374b7ca3a92e0b61b2f4a26a8a73bf8aebec17e7
TRalexxx <110291348+TRalexxx@users.noreply.github.com>
2023-04-02 12:50:04 +0300

    Replaced creation buttons into Form1.cs, added buttons with winner info and starting game again.

--- XOgame/Form1.cs
+++ XOgame/Form1.cs
@@ -16,5 +16,290 @@ namespace XOgame
         {
             InitializeComponent();
         }
+
+        private void CreateField(object sender, System.EventArgs e)
+        {
+            bool click = true;
+            int posX = 0;
+            int posY = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(100, 100);
+                btn.Location = new Point(posX, posY);
+                btn.Font = new Font("Times New Roman", 28);
+                btn.Click += (ev, sendr) =>
+                {
+                    if (click) btn.Text = "X";
+                    else btn.Text = "0";
+                    click = !click;
+                    btn.Enabled = false;
+                    if (this.Controls[0].Text.Equals("X") && this.Controls[1].Text.Equals("X") && this.Controls[2].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[0].BackColor = Color.YellowGreen;
+                        this.Controls[1].BackColor = Color.YellowGreen;
+                        this.Controls[2].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if(MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) 
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[0].Text.Equals("X") && this.Controls[4].Text.Equals("X") && this.Controls[8].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[0].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[8].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[0].Text.Equals("X") && this.Controls[3].Text.Equals("X") && this.Controls[6].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[0].BackColor = Color.YellowGreen;
+                        this.Controls[3].BackColor = Color.YellowGreen;
+                        this.Controls[6].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[0].Text.Equals("0") && this.Controls[1].Text.Equals("0") && this.Controls[2].Text.Equals("0"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[0].BackColor = Color.YellowGreen;
+                        this.Controls[1].BackColor = Color.YellowGreen;
+                        this.Controls[2].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[0].Text.Equals("0") && this.Controls[4].Text.Equals("0") && this.Controls[8].Text.Equals("0"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[0].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[8].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[0].Text.Equals("0") && this.Controls[3].Text.Equals("0") && this.Controls[6].Text.Equals("0"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[0].BackColor = Color.YellowGreen;
+                        this.Controls[3].BackColor = Color.YellowGreen;
+                        this.Controls[6].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }                    
+                    else if (this.Controls[1].Text.Equals("X") && this.Controls[4].Text.Equals("X") && this.Controls[7].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[1].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[7].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }                    
+                    else if (this.Controls[1].Text.Equals("0") && this.Controls[4].Text.Equals("0") && this.Controls[7].Text.Equals("0"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[1].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[7].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }                   
+                    else if (this.Controls[2].Text.Equals("X") && this.Controls[4].Text.Equals("X") && this.Controls[6].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[2].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[6].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[2].Text.Equals("X") && this.Controls[5].Text.Equals("X") && this.Controls[8].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[2].BackColor = Color.YellowGreen;
+                        this.Controls[5].BackColor = Color.YellowGreen;
+                        this.Controls[8].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }                    
+                    else if (this.Controls[2].Text.Equals("0") && this.Controls[4].Text.Equals("0") && this.Controls[6].Text.Equals("0"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[2].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[6].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[2].Text.Equals("0") && this.Controls[5].Text.Equals("0") && this.Controls[8].Text.Equals("0"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[2].BackColor = Color.YellowGreen;
+                        this.Controls[5].BackColor = Color.YellowGreen;
+                        this.Controls[8].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[3].Text.Equals("X") && this.Controls[4].Text.Equals("X") && this.Controls[5].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[3].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[5].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }                    
+                    else if (this.Controls[3].Text.Equals("0") && this.Controls[4].Text.Equals("0") && this.Controls[5].Text.Equals("0"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[3].BackColor = Color.YellowGreen;
+                        this.Controls[4].BackColor = Color.YellowGreen;
+                        this.Controls[5].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[6].Text.Equals("X") && this.Controls[7].Text.Equals("X") && this.Controls[8].Text.Equals("X"))
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[6].BackColor = Color.YellowGreen;
+                        this.Controls[7].BackColor = Color.YellowGreen;
+                        this.Controls[8].BackColor = Color.YellowGreen;
+                        MessageBox.Show("X Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                    else if (this.Controls[6].Text.Equals("0") && this.Controls[7].Text.Equals("0") && this.Controls[8].Text.Equals("0"))
+                    {
+                        
+                        for (int j = 0; j < 9; j++)
+                        {
+                            this.Controls[j].Enabled = false;
+                        }
+                        this.Controls[6].BackColor = Color.YellowGreen;
+                        this.Controls[7].BackColor = Color.YellowGreen;
+                        this.Controls[8].BackColor = Color.YellowGreen;
+                        MessageBox.Show("O Wins", "Caption", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (MessageBox.Show("Play again", "Caprion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            this.Controls.Clear();
+                            CreateField(sender, e);
+                        }
+                    }
+                };
+                this.Controls.Add(btn);
+                if (posX >= 200)
+                {
+                    posX = 0;
+                    posY += 100;
+                }
+                else posX += 100;
+            }
+        }
     }
 }
